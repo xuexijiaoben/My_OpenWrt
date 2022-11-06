7c45f00ba81acfc4e606aee887d963c4fd15106d
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2022-11-06 11:59:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,11 +20,19 @@ echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf
 echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic.git' >>feeds.conf.default
 echo 'src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git' >>feeds.conf.default
 
+# 科学上网插件
+git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+svn co https://github.com/kiddin9/openwrt-bypass/trunk/luci-app-bypass package/luci-app-bypass
+
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
-
+# rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon-18.06
+git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/themes/luci-app-argon-config
+# echo '### Argon Theme Config ###'
 
 # rm -rf package/luci-app-ikoolproxy
 # rm -rf package/luci-app-ikoolproxy2
@@ -32,10 +40,7 @@ echo 'src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cd
 
 svn co https://github.com/sbwml/luci-app-alist/trunk/luci-app-alist  package/luci-app-alist
 
-# rm -rf package/feeds/luci/luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-# echo '### Argon Theme Config ###'
+
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
