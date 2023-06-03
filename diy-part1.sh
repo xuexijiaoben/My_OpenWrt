eb663f4cc0916872a8c5dde9a2b71860971a051e
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2023-06-03 21:50:40 +0800

    istore

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,7 @@
 # sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages ' feeds.conf.default
 
 # Add a feed source
 echo 'src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git' >>feeds.conf.default
