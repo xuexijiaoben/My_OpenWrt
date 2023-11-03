d17b909d041109dcdfa7f7b8ae4369fd6690a237
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2023-11-03 20:30:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,41 +28,26 @@ svn co https://github.com/linkease/nas-packages/trunk/network/services/quickstar
 
 # 科学上网插件
 # git clone --depth 1 https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
-echo 'src-git ssrplus https://github.com/fw876/helloworld' >>feeds.conf.default
 
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-
-# svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
-# svn co https://github.com/kenzok8/small-package/trunk/luci-app-passwall package/luci-app-passwall
+echo 'src-git ssrplus https://github.com/fw876/helloworld.git;main' >>feeds.conf.default
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-bypass package/luci-app-bypass
 svn co https://github.com/kenzok8/small-package/trunk/lua-neturl package/lua-neturl
 svn co https://github.com/kenzok8/small-package/trunk/redsocks2 package/redsocks2
 git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 
+# svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
+# svn co https://github.com/kenzok8/small-package/trunk/luci-app-passwall package/luci-app-passwall
 # kenzok8 inclued lua-maxminddb
 # git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-
 # echo 'src-git lucky https://github.com/sirpdboy/luci-app-lucky' >>feeds.conf.default
 # echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 
 # 添加额外软件包
 # echo 'src-git alist https://github.com/sbwml/luci-app-alist' >>feeds.conf.default
 # echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic.git' >>feeds.conf.default
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/adguardhome
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/filebrowser package/filebrowser
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-filebrowser package/luci-app-filebrowser
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/luci-app-smartdns
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-store package/luci-app-store
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-lib-taskd package/luci-lib-taskd
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-lib-xterm package/luci-lib-xterm
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns package/smartdns
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/taskd package/taskd
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/v2ray-geodata package/v2ray-geodata
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/UnblockNeteaseMusic package/UnblockNeteaseMusic
 # git clone --depth 1  https://github.com/ilxp/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
 
 # 主题
