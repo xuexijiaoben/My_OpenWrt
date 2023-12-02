e67ca7e92c274038c9f8397569316ab034e1280e
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2023-12-02 21:34:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,8 +22,8 @@ sed -i 's/OpenWrt/Phicomm_N1/g' package/base-files/files/bin/config_generate
 # 5.修改默认主题
 # sed -i ' s/luci-theme-bootstrap/luci-theme-argon/g ' feeds/luci/collections/luci/Makefile
 # 或
-default_theme='opentomcat'
-sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+# default_theme='opentomcat'
+# sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
 # 6.设置ttyd免登录
 # sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd
@@ -38,25 +38,25 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package
 
 # 修改晶晨宝盒默认配置
 # 1.Set the download repository of the OpenWrt files to your github.com
-# sed -i "s|https.*/OpenWrt|https://github.com/xuexijiaoben/My-N1-shangyou-dabao|g" feeds/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+# sed -i "s|https.*/OpenWrt|https://github.com/xuexijiaoben/My-N1-shangyou-dabao|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
 # 2.Set the keywords of Tags in your github.com Releases
-# sed -i "s|ARMv8|armvirt|g" feeds/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+# sed -i "s|ARMv8|armvirt|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
 # 3.Set the suffix of the OPENWRT files in your github.com Releases
-# sed -i "s|.img.gz|.img.gz|g" feeds/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+# sed -i "s|.img.gz|.img.gz|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
 # 4.Set the download path of the kernel in your github.com repository
-# sed -i "s|opt/kernel|opt/kernel|g" feeds/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+# sed -i "s|opt/kernel|opt/kernel|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
 
 # 1.设置OpenWrt 文件的下载仓库
-sed -i "s|amlogic_firmware_repo.*|amlogic_firmware_repo 'https://github.com/xuexijiaoben/My-N1-shangyou-dabao'|g" feeds/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|amlogic_firmware_repo.*|amlogic_firmware_repo 'https://github.com/xuexijiaoben/My-N1-shangyou-dabao'|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
 
 # 2.设置 Releases 里 Tags 的关键字
-sed -i "s|ARMv8|armvirt|g" feeds/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|ARMv8|armvirt|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
 
 # 3.设置 Releases 里 OpenWrt 文件的后缀
-# sed -i "s|.img.gz|.OPENWRT_SUFFIX|g" feeds/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+# sed -i "s|.img.gz|.OPENWRT_SUFFIX|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
 
 # 4.设置 OpenWrt 内核的下载路径
-sed -i "s|amlogic_kernel_path.*|amlogic_kernel_path 'https://github.com/breakings/OpenWrt'|g" feeds/kenzok8/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|amlogic_kernel_path.*|amlogic_kernel_path 'https://github.com/breakings/OpenWrt'|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
 
 
 # echo '修改时区'
