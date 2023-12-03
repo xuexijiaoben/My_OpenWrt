53463374ed895334ed25393b49ed0a8db1eca5ce
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2023-12-03 15:12:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -84,4 +84,5 @@ rm -rf feeds/luci/applications/luci-app-argon-config # if have
 git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon-18.06
 
+./scripts/feeds update -a
 ./scripts/feeds install -a
