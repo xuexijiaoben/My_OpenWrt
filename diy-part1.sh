b646d750808754284a62252207cbab818e3470ec
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2024-02-01 17:03:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -64,7 +64,7 @@ git clone -b master --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-un
 
 # Add a feed source
 # 添加额外软件包
-# git clone --depth 1  https://github.com/ilxp/luci-app-ikoolproxy.git package/app/luci-app-ikoolproxy
+git clone --depth 1  https://github.com/ilxp/luci-app-ikoolproxy.git package/app/luci-app-ikoolproxy
 echo 'src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git' >>feeds.conf.default
 echo 'src-git cloudflarespeedtest https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest.git' >>feeds.conf.default
 
